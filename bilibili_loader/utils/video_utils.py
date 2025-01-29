dbe27870aaa33467240817995d4f69f0a8467e7f
Fallingnight <fallingnight131@outlook.com>
2025-01-30 05:19:04 +0800

    路径不存在则自动创建

--- bilibili_loader/utils/video_utils.py
+++ bilibili_loader/utils/video_utils.py
@@ -1,17 +1,21 @@
 import os
 from moviepy.editor import VideoFileClip, AudioFileClip
 
-def merge_video(video_path = "bilibili_loader/cache/video/video.mp4", 
-                audio_path = "bilibili_loader/cache/audio/audio.m4a", 
-                output_path = "bilibili_loader/cache/output",
-                output_name = "output"):
+def merge_video(video_path="bilibili_loader/cache/video/video.mp4", 
+                audio_path="bilibili_loader/cache/audio/audio.m4a", 
+                output_path="bilibili_loader/cache/output",
+                output_name="output"):
     """合并视频和音频文件。"""
     try:
+        # 确保输出目录存在
+        os.makedirs(output_path, exist_ok=True)
+        
         video = VideoFileClip(video_path)
         audio = AudioFileClip(audio_path)
         video = video.set_audio(audio)
-        video.write_videofile(f"{output_path}/{output_name}.mp4", codec="libx264", audio_codec="aac")
-        return f"{output_path}/{output_name}.mp4"
+        output_file = f"{output_path}/{output_name}.mp4"
+        video.write_videofile(output_file, codec="libx264", audio_codec="aac")
+        return output_file
     except Exception as e:
         print(f"合并失败: {e}")
         return None
